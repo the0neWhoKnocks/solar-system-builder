b9de04edd3d72c90191e7d4c988afd05c2113482
Trevor Lemon <duhlemman@gmail.com>
2019-08-27 17:44:22 -0700

    Added the grid and some styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+import Grid from 'COMPONENTS/Grid';
+
+const styles = document.createElement('style');
+styles.innerHTML = `
+  html, body {
+    padding: 0;
+    margin: 0;
+  }
+  body {
+    background: #000;
+  }
+  
+  .bg-grid {
+    background: #262d38;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }  
+`;
+document.head.append(styles);
+
 document.addEventListener('DOMContentLoaded', () => {
-  alert('hello');
+  document.body.append(new Grid({
+    className: 'bg-grid',
+    lineColor: 'rgba(255, 255, 255, 0.1)',
+  }));
 });
