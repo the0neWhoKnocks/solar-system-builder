b57203ce32e6d3eb8956c9435df0bb0d235d0d3a
tlemon <trevor.lemon@nike.com>
2019-11-11 11:26:25 -0800

    Styles per-component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Grid from 'COMPONENTS/Grid';
+import addStyles from 'UTILS/addStyles';
 
-const styles = document.createElement('style');
-styles.innerHTML = `
+addStyles('rootStyles', `
   html, body {
     padding: 0;
     margin: 0;
@@ -9,25 +9,7 @@ styles.innerHTML = `
   body {
     background: #000;
   }
-  
-  .bg-grid {
-    font-size: 0;
-    background: #262d38;
-    cursor: crosshair;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }  
-  .bg-grid .hover-canvas {
-    filter: blur(14px);
-    opacity: 0.3;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }  
-`;
-document.head.append(styles);
+`);
 
 document.addEventListener('DOMContentLoaded', () => {
   document.body.append(new Grid({
