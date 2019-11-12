29e8b2728ef2060ef3c1ea4214921e24ddb5c1fe
tlemon <trevor.lemon@nike.com>
2019-11-12 15:58:57 -0800

    Can now edit CB props

--- src/components/Creator/index.js
+++ src/components/Creator/index.js
@@ -14,7 +14,8 @@ export default class Creator {
       }
     `);
     
-    this.planets = {};
+    this.celestialBodies = {};
+    this.parentEl = parentEl;
     
     this.parentSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     this.parentSVG.setAttributeNS(null, 'class', className);
@@ -26,49 +27,27 @@ export default class Creator {
     this.mouseUp = this.mouseUp.bind(this);
     
     this.handleClick = this.handleClick.bind(this);
-    this.handleContextMenu = this.handleContextMenu.bind(this);
-    this.handleMouseOver = this.handleMouseOver.bind(this);
     
     this.addListeners();
   }
   
   addListeners() {
     this.parentSVG.addEventListener('click', this.handleClick);
-    this.parentSVG.addEventListener('contextmenu', this.handleContextMenu);
-    // this.parentSVG.addEventListener('mouseover', this.handleMouseOver);
   }
   
   handleClick(ev) {
     const el = ev.target;
     
     if(!this.mouseIsDown && el.nodeName !== 'svg'){
-      console.log('click', this.planets[el.getAttribute('data-id')]);
-    }
-  }
-  
-  handleContextMenu(ev) {
-    ev.preventDefault();
-    console.log('menu', ev.target);
-  }
-  
-  handleMouseOver(ev) {
-    const el = ev.target;
-    
-    if(!this.mouseIsDown && el.nodeName !== 'svg'){
-      console.log('over', this.planets[el.getAttribute('data-id')]);
+      const cB = this.celestialBodies[el.getAttribute('data-id')];
+      cB.editAttributes({ parentEl: this.parentEl });
     }
   }
   
   mouseDown({ mouseX, mouseY }) {
     this.startX = mouseX;
     this.startY = mouseY;
-    this.currentCelestialBody = new CelestialBody({
-      parentEl: this.parentSVG,
-      x: this.startX,
-      y: this.startY,
-    });
     this.mouseIsDown = true;
-    this.planets[this.currentCelestialBody.id] = this.currentCelestialBody;
   }
   
   mouseMoved({ mouseX, mouseY }) {
@@ -76,11 +55,23 @@ export default class Creator {
       const diffX = mouseX - this.startX;
       const diffY = mouseY - this.startY;
       const radius = Math.sqrt( (diffX * diffX) + (diffY * diffY) );
-      this.currentCelestialBody.setRadius(radius);
+      
+      if(!this.currentCelestialBody && radius >= CelestialBody.SIZE__MIN){
+        this.currentCelestialBody = new CelestialBody({
+          parentEl: this.parentSVG,
+          x: this.startX,
+          y: this.startY,
+        });
+        this.celestialBodies[this.currentCelestialBody.id] = this.currentCelestialBody;
+      }
+      else if(this.currentCelestialBody){
+        this.currentCelestialBody.setRadius(radius);
+      }
     }
   }
   
   mouseUp() {
+    this.currentCelestialBody = undefined;
     this.mouseIsDown = false;
   }
 }
