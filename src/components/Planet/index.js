a95fa2d407b9497168090b1a6e3303373bc35376
Nox <the0neWhoKnocks@users.noreply.github.com>
2019-11-12 12:01:01 -0800

    Generate planets with random color
    Ditched the gradient for the planets for now. Going with a simpler 
    approach that'll just render flat circles.

--- src/components/Planet/index.js
+++ src/components/Planet/index.js
@@ -1,17 +1,22 @@
 export default class Planet {
+  static genColor() {
+    return `#${ ((1<<24)*Math.random()|0).toString(16) }`;
+  }
+  
   constructor({
-    color = '#CCC',
+    color,
     parentEl,
     radius = 0,
     x = 0,
     y = 0,
   } = {}) {
+    this.color = color || Planet.genColor();
+    
     this.planet = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
     this.planet.setAttributeNS(null, 'cx', x);
     this.planet.setAttributeNS(null, 'cy', y);
     this.planet.setAttributeNS(null, 'r', radius);
-    this.planet.setAttributeNS(null, 'fill', 'url(#radGrad)');
-    // this.planet.setAttributeNS(null, 'fill', color);
+    this.planet.setAttributeNS(null, 'fill', this.color);
     this.planet.setAttributeNS(null, 'stroke', 'none');
     
     parentEl.appendChild(this.planet);
