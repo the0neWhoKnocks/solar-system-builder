13cab57396d77b4eb77a4e359b6d1b47c2cace76
Nox <the0neWhoKnocks@users.noreply.github.com>
2019-11-12 14:05:42 -0800

    Began work on item menus

--- src/components/PlanetCreator/index.js
+++ src/components/PlanetCreator/index.js
@@ -14,6 +14,8 @@ export default class PlanetCreator {
       }
     `);
     
+    this.planets = {};
+    
     this.parentSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     this.parentSVG.setAttributeNS(null, 'class', className);
     this.parentSVG.setAttributeNS(null, 'width', parentEl.offsetWidth);
@@ -22,17 +24,51 @@ export default class PlanetCreator {
     this.mouseDown = this.mouseDown.bind(this);
     this.mouseMoved = this.mouseMoved.bind(this);
     this.mouseUp = this.mouseUp.bind(this);
+    
+    this.handleClick = this.handleClick.bind(this);
+    this.handleContextMenu = this.handleContextMenu.bind(this);
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    
+    this.addListeners();
+  }
+  
+  addListeners() {
+    this.parentSVG.addEventListener('click', this.handleClick);
+    this.parentSVG.addEventListener('contextmenu', this.handleContextMenu);
+    // this.parentSVG.addEventListener('mouseover', this.handleMouseOver);
+  }
+  
+  handleClick(ev) {
+    const el = ev.target;
+    
+    if(!this.mouseIsDown && el.nodeName !== 'svg'){
+      console.log('click', this.planets[el.getAttribute('data-id')]);
+    }
+  }
+  
+  handleContextMenu(ev) {
+    ev.preventDefault();
+    console.log('menu', ev.target);
+  }
+  
+  handleMouseOver(ev) {
+    const el = ev.target;
+    
+    if(!this.mouseIsDown && el.nodeName !== 'svg'){
+      console.log('over', this.planets[el.getAttribute('data-id')]);
+    }
   }
   
   mouseDown({ mouseX, mouseY }) {
     this.startX = mouseX;
     this.startY = mouseY;
-    this.planet = new Planet({
+    this.currentPlanet = new Planet({
       parentEl: this.parentSVG,
       x: this.startX,
       y: this.startY,
     });
     this.mouseIsDown = true;
+    this.planets[this.currentPlanet.id] = this.currentPlanet;
   }
   
   mouseMoved({ mouseX, mouseY }) {
@@ -40,7 +76,7 @@ export default class PlanetCreator {
       const diffX = mouseX - this.startX;
       const diffY = mouseY - this.startY;
       const radius = Math.sqrt( (diffX * diffX) + (diffY * diffY) );
-      this.planet.setRadius(radius);
+      this.currentPlanet.setRadius(radius);
     }
   }
   
