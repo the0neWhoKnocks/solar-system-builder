62269a8991cd94f20620356374942a686bcb7bd5
tlemon <trevor.lemon@nike.com>
2019-11-15 11:58:04 -0800

    Moved glow filter to Sun

--- src/components/Sun/index.js
+++ src/components/Sun/index.js
@@ -3,9 +3,10 @@ import CelestialBody from 'COMPONENTS/CelestialBody';
 export default class Sun extends CelestialBody {
   constructor(opts) {
     opts.color = '#FFFFFF';
-    opts.filter = 'glow';
     super(opts);
     
     delete this.editableProps.color;
+    
+    this.celestialBody.setAttributeNS(null, 'filter', 'url(#glow)');
   }
 }
